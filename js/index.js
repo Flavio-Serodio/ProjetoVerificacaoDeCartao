ddf8d4d498ab10f2cea6999ceab85c36ca19a7f4
Flavio <flaviohgserodio@hotmail.com>
2026-03-03 00:01:58 -0300

    feat: js fazendo a funcao de atualizacao do span na imagen do cartao

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
 const inputName =  document.getElementById("CardholderName")
+const inputNumber = document.getElementById("inputCardNumber")
+const inputMM = document.getElementById("MM")
+const inputYY = document.getElementById("YY")
+const inputCVC = document.getElementById("CVC")
+const cardNumber = document.querySelector(".cardNumber")
+const cardName = document.querySelector(".nameFront")
+const cardDateMM = document.querySelector(".dataFrontMM")
+const cardDateYY = document.querySelector(".dataFrontYY")
+const cardCVC = document.querySelector(".CVC-image")
+
+function updateCard(input,element,defaltValue){
+    element.textContent = input.value || defaltValue
+}
+
+function updateCardNumber(input, element, defaltValue){
+    input.addEventListener("input",() => {
+        input.value = input.value.replace(/\D/g, "")
+        element.textContent = input.value || defaltValue;
+    })
+}
+
+
+updateCard(inputName, cardName, "Jane Appleseed");
+updateCardNumber(inputMM, cardDateMM, "00");
+updateCardNumber(inputYY, cardDateYY, "00");
+updateCardNumber(inputCVC, cardCVC, "000")
+
+inputNumber.addEventListener("input", function () {
+
+  // Remove tudo que não for número
+  let value = inputNumber.value.replace(/\D/g, "")
+
+  // Limita a 16 números
+  value = value.slice(0, 16)
+
+  // Adiciona espaço a cada 4 números
+  value = value.replace(/(\d{4})(?=\d)/g, "$1 ")
+
+  inputNumber.value = value
+    cardNumber.textContent = value || "0000 0000 0000 0000"
+  
+})
